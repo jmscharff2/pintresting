a2f294faaa7fe0db5a7bb93102ef809b1ffc3821
jmscharff2 <jscharff2@gmail.com>
2014-10-06 13:34:03 -0400

    changed email to users name on the pin page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
 
          has_many :pins
+
+   validates :name, presence: true
+   validates :image, presence: true
+   validates :description, presence: true
 end
